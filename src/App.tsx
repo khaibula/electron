635a01a6da23fb0b61092d56ce5e8599b94d7d2c
khaibula <gaziev.777@bk.ru>
2023-06-30 22:58:47 +0300

    add load file

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,38 @@ import { FileIcon } from "./FileIcon.tsx";
 function App() {
   const [inputValue, setInputValue] = useState('')
 
+  const chooseFileHandler = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.mp4'; // Можете изменить тип файла или оставить "*/*" для любого типа файла
+
+    input.onchange = (event: Event) => {
+      // @ts-ignore
+      const file = event?.target?.files?.[0];
+      console.log('file', file)
+    };
+
+    input.click();
+  }
+
   return (
     <div className="bg-gray-800 flex justify-center items-end h-screen p-10">
       <div className="relative max-w-4xl w-full">
         <input
           type="text"
           placeholder="Send a message"
-          className="w-full px-4 py-4 shadow-xl bg-gray-700 rounded-md focus:outline text-white"
+          className="w-full px-4 py-4 pl-10 shadow-xl bg-gray-700 rounded-md focus:outline text-white outline-none"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
         <button
-          className="absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 enabled:bg-brand-purple text-white bottom-1.5 transition-colors disabled:opacity-40"
+          className="absolute p-1 left-2 bottom-4 transition-colors"
+          onClick={chooseFileHandler}
         >
           <FileIcon />
         </button>
         <button
-          className="absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 enabled:bg-brand-purple text-white bottom-1.5 transition-colors disabled:opacity-40"
+          className="absolute p-1 right-2 bottom-4 transition-colors"
         >
           <SendIcon />
         </button>
